58da21c8bbda7c1585bf80758d8b3bad48a433e3
satofumi0623 <satofumi0623@yahoo.co.jp>
2026-08-25 11:56:11 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/career-consultant-quiz-400/' : '/',
+  base: mode === 'production' ? '/career-consultant-quiz-400-v2/' : '/',
 }))
